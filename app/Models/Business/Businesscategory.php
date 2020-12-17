51fb609e62b1010561955b415f74a3705f17b996
tamy86 <34414031+tamy86@users.noreply.github.com>
2020-12-17 13:39:01 -0800

    add controller file(loginBusiness), add Model Businesscategory,add Migration (create business category)-28/09/1399->commit no=4

---
+++ app/Models/Business/Businesscategory.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\Business;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Businesscategory extends Model
+{
+    use HasFactory;
+}
