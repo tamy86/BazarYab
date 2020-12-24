c642f2fbb2f964616a8fdc09677532d575bd417f
tamy86 <34414031+tamy86@users.noreply.github.com>
2020-12-24 13:24:05 -0800

    corection the name of migration (insert_customer_user) and fill db lists04/10/1399->commit no=8

--- database/migrations/2020_12_24_204719_insert_customer_user.php
+++ database/migrations/2020_12_24_204719_insert_customer_user.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class InsertUstomerUser extends Migration
+class InsertCustomerUser extends Migration
 {
     /**
      * Run the migrations.
@@ -14,7 +14,7 @@ class InsertUstomerUser extends Migration
     public function up()
     {
         Schema::create('customerUsers', function (Blueprint $table) {
-            $table->id();
+            $table->id()->autoIncrement()->index();
             $table->string('ipaddress')->index();
             $table->string('phone')->index();
             $table->string('verify')->index();
